cb3dbbe9f512c380bd7acdd4431fd5516a1e4c62
keishi.ishihara <keishi.ishihara17@gmail.com>
2025-06-17 19:20:29 +0900

    Add carla_garage

---
+++ packages/carla_garage/leaderboard_autopilot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="leaderboard",
+    version="0.9.15",
+    packages=find_packages(),
+    description="CARLA Leaderboard (Autopilot)",
+    author="CARLA Simulator Team",
+    license="MIT",
+)
---
+++ packages/carla_garage/scenario_runner_autopilot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="srunner",
+    version="0.9.15",
+    packages=find_packages(),
+    description="Scenario Runner for CARLA",
+    author="CARLA Simulator Team",
+    license="MIT",
+)
